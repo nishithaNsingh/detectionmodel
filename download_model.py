6baf37adb5fdb6385360295de4a97cc6ffe3db50
Nishitha Singh N <nishithasingh777@gmail.com>
2025-11-13 07:50:23 +0530

    up

--- download_model.py
+++ download_model.py
@@ -1,5 +1,10 @@
 import gdown
 
-url = "https://drive.google.com/file/d/1rKh-IElSdHTqax7XdfSdZTn-r8T_qWPf/viewc"
+# Direct link for gdown
+url = "https://drive.google.com/uc?id=1rKh-IElSdHTqax7XdfSdZTn-r8T_qWPf"
+
 output = "plant_disease_prediction_model.h5"
+
+print("Downloading model...")
 gdown.download(url, output, quiet=False)
+print("Model downloaded successfully!")
